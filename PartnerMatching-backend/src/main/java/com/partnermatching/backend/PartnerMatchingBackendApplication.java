04d77109bed4aa6d1db965f49c437be7c4fcf905
LJW <lijingwei0316@163.com>
2024-02-19 18:27:28 +0800

    backend init

---
+++ PartnerMatching-backend/src/main/java/com/partnermatching/backend/PartnerMatchingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.partnermatching.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerMatchingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerMatchingBackendApplication.class, args);
+    }
+
+}
---
+++ PartnerMatching-backend/src/test/java/com/partnermatching/backend/PartnerMatchingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partnermatching.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerMatchingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
